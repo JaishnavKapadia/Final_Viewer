bb6440bb6cd00c814a5087e956ffc5531f49d7dc
Jaishnav Kapadia <kapjaishnav@gmail.com>
2025-04-04 18:57:11 -0400

    removed unused imports

--- modelviewer/src/App.tsx
+++ modelviewer/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF, Environment } from "@react-three/drei";
 import { Button } from "@/components/ui/button";
